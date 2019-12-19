9859d288a722719b1ff979f118da972581532e08
Dylancarroll <55232738+Dylancarroll@users.noreply.github.com>
2019-12-19 11:48:42 +0000

    Add files via upload

---
+++ js/switchcase.js
@@ -0,0 +1,23 @@
+function checkvalue(){
+var text;
+var lang= document.getelementbyid("input value").value;
+
+Switch(age) {
+  case"apple":
+  text="There are over 7500 varities of apples around the world";
+  break;
+  case"pear":
+  text="There are over 3000 varities of pears"
+  break;
+  case"orange":
+  text="nothing rhymes with me"
+  break;
+  case"Bannana"
+  text="full of potasium"
+  break;
+
+default:
+text="plss enter text!";
+}
+document.getElementId("displayText").innerHTML=text;
+}
